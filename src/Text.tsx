a605200968e653786fc794d29b576a1dd3d63ceb
haryu <67352550+koteharyu@users.noreply.github.com>
2021-09-22 14:09:52 +0900

    Initial commit

---
+++ src/Text.tsx
@@ -0,0 +1,15 @@
+import { VFC } from "react";
+
+type Props = {
+  color: string;
+  fontSize: string;
+};
+
+export const Text: VFC<Props> = (props) => {
+  const { color, fontSize } = props;
+  return <p style={{ color, fontSize }}>TEXT.ts</p>;
+};
+
+// FC == Function Component
+
+// VFC: childrenを含まないFC
